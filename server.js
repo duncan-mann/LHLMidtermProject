1d09efd292b35873453d45dbb959c5991d0ac58f
Mike Nguyen <peps.minhnguyen@gmail.com>
2019-10-19 21:33:24 +0000

    add all css, html, scss

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+// const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+// app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
@@ -47,6 +47,14 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
+  res.render("register");
+});
+
+app.get("/todos", (req, res) => {
+  res.render("todos");
+});
+
+app.get("/home", (req, res) => {
   res.render("index");
 });
 
