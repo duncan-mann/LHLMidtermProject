73e28942ef02bd8c9a626b36862fddf61963f065
vagrant <duncan.s.mann@gmail.com>
2019-10-19 20:43:29 +0000

    Added getUserToDos to module.exports of app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,7 +11,16 @@
 
 const db = require('../database.js');
 
-const getToDosByUser = function(user_id) {
+const getUserToDos = function(user_id) {
 
-  
+  return db.query(`
+  SELECT *
+  FROM to_dos
+  WHERE user_id = $1
+  `, [user_id])
+  .then(res => console.log(res.rows))
+  .catch(e => console.error('query error: ', e.stack))
+
 }
+
+module.exports.getUserToDos = getUserToDos;
