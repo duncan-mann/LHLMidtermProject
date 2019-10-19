53a7c357f8f9236cb1ede81047d5cf15b16030db
vagrant <duncan.s.mann@gmail.com>
2019-10-19 20:41:18 +0000

    Required database.js in app.js script. Started creating getToDosByUser function.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,17 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });;
+// });
+
+const db = require('../database.js');
+
+const getToDosByUser = function(user_id) {
+
+  
+}
