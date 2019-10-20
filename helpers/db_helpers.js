2c25f667bb56e397abae0269522a0d7e4687c5dc
Mike Nguyen <peps.minhnguyen@gmail.com>
2019-10-20 18:19:58 +0000

    add register function

--- helpers/db_helpers.js
+++ helpers/db_helpers.js
@@ -7,9 +7,16 @@ const getUserToDos = function(user_id) {
   FROM to_dos
   WHERE user_id = $1
   `, [user_id])
-  .then(res => console.log(res.rows))
-  .catch(e => console.error('query error: ', e.stack))
+  .then(res => res.rows)
+  .catch(e => null)
 
 }
+
+  const addUser = function (user) {
+    const insertString = `INSERT INTO users(name, email, password) VALUEs ($1, $2, $3)`
+    return db.query (insertString, [user.name, user.email, user.password])
+    .then(res => console.log(res.rows))
+    .catch(e => console.error('query error: ', e.stack))
+  }
 
 module.exports.getUserToDos = getUserToDos;
