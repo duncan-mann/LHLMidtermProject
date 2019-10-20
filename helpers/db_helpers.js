0218f5649fd75ad9bf48e9b38ead10e83274c396
vagrant <duncan.s.mann@gmail.com>
2019-10-20 18:50:17 +0000

    Create getUserByemail function

--- helpers/db_helpers.js
+++ helpers/db_helpers.js
@@ -11,5 +11,21 @@ const getUserToDos = function(user_id) {
   .catch(e => console.error('query error: ', e.stack))
 
 }
-
 module.exports.getUserToDos = getUserToDos;
+
+const getUserByEmail = function(userEmail) {
+// This function takes in an email, and then returns an object 'user', with the corresponding email and password of that user. 
+// Server will verifiy that the password matches in POST /login request. 
+  return db.query(
+    `SELECT *
+    FROM users
+    WHERE email = $1`
+  [userEmail]).then(res => {
+    const user = {
+      email = res.rows[0].email,
+      password = res.rows[0].password
+    }
+    return user})
+  .catch(e => console.log('User could not be found: ', e.stack))
+}
+module.exports.getUsersByEmail = getUsersByEmail;
