c1b2cc76c07e8000eccc09aaecea75524e49c563
vagrant <duncan.s.mann@gmail.com>
2019-10-21 14:48:12 +0000

    Fixed IDs in user seeds for registration

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,11 +1,11 @@
 -- Users table seeds here (Example)
-INSERT INTO users (id, username, first_name, last_name, email, password) VALUES (1, 'dmann', 'Duncan', 'Mann', 'duncan@mann.com', 'password');
-INSERT INTO users (id, username, first_name, last_name, email, password) VALUES (2, 'lxg1992', 'Alex', 'Garey', 'alex@garey.com', 'password');
-INSERT INTO users (id, username, first_name, last_name, email, password) VALUES (3, 'mike123', 'Mike', 'Nguyen', 'mike@nguyen.com', 'password');
+INSERT INTO users (username, first_name, last_name, email, password) VALUES ('dmann', 'Duncan', 'Mann', 'duncan@mann.com', 'password');
+INSERT INTO users (username, first_name, last_name, email, password) VALUES ('lxg1992', 'Alex', 'Garey', 'alex@garey.com', 'password');
+INSERT INTO users (username, first_name, last_name, email, password) VALUES ('mike123', 'Mike', 'Nguyen', 'mike@nguyen.com', 'password');
 
-INSERT INTO to_dos (id, user_id, description, category) VALUES (1, 1, 'harry potter', 'movies');
-INSERT INTO to_dos (id, user_id, description, category) VALUES (2, 1, 'to kill a mockingbird', 'books');
-INSERT INTO to_dos (id, user_id, description, category) VALUES (3, 2, 'the keg', 'restaurant');
-INSERT INTO to_dos (id, user_id, description, category) VALUES (4, 2, 'macbook', 'product');
-INSERT INTO to_dos (id, user_id, description, category) VALUES (5, 3, 'breaking bad', 'movies');
-INSERT INTO to_dos (id, user_id, description, category) VALUES (6, 3, 'sapiens', 'books');
+INSERT INTO to_dos (user_id, description, category) VALUES (1, 'harry potter', 'movies');
+INSERT INTO to_dos (user_id, description, category) VALUES (1, 'to kill a mockingbird', 'books');
+INSERT INTO to_dos (user_id, description, category) VALUES (2, 'the keg', 'restaurant');
+INSERT INTO to_dos (user_id, description, category) VALUES (2, 'macbook', 'product');
+INSERT INTO to_dos (user_id, description, category) VALUES (3, 'breaking bad', 'movies');
+INSERT INTO to_dos (user_id, description, category) VALUES (3, 'sapiens', 'books');
