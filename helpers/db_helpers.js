1bf230fcd32fab347b38684263eed38db1b34b7c
vagrant <duncan.s.mann@gmail.com>
2019-10-21 14:57:44 +0000

    Fixed merging of db_helpers

--- helpers/db_helpers.js
+++ helpers/db_helpers.js
@@ -30,3 +30,34 @@ module.exports.getUserToDos = getUserToDos;
 module.exports.getUserToDos = getUserToDos;
 module.exports.addUser = addUser ;
 module.exports.checkEmailandUser = checkEmailandUser;
+// This function takes in an email, and then returns an object 'user', with the corresponding email and password of that user. 
+// Server will verifiy that the password matches in POST /login request. 
+
+const getUserByEmail = function(userEmail) {
+
+  return db.query(`
+    SELECT *
+    FROM users
+    `)
+    .then(res => {
+      for (each of res.rows) {
+        if (each.email === userEmail) {
+          return each;
+        }
+      }
+    return undefined})
+  .catch(e => console.log('Error finding user', e.stack))
+}
+module.exports.getUserByEmail = getUserByEmail;
+
+const generateRandomString = function() {
+  let result           = '';
+  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+module.exports.generateRandomString = generateRandomString;
