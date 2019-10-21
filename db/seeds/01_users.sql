1859c194d61a86ec952d055ddc987db3324ab779
vagrant <duncan.s.mann@gmail.com>
2019-10-21 18:17:24 +0000

    Added seeds to work on styling. Add flexbox to todo-items

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -3,9 +3,13 @@ INSERT INTO users (username, first_name, last_name, email, password) VALUES ('dm
 INSERT INTO users (username, first_name, last_name, email, password) VALUES ('lxg1992', 'Alex', 'Garey', 'alex@garey.com', 'password');
 INSERT INTO users (username, first_name, last_name, email, password) VALUES ('mike123', 'Mike', 'Nguyen', 'mike@nguyen.com', 'password');
 
-INSERT INTO to_dos (user_id, description, category) VALUES (1, 'harry potter', 'movies');
-INSERT INTO to_dos (user_id, description, category) VALUES (1, 'to kill a mockingbird', 'books');
-INSERT INTO to_dos (user_id, description, category) VALUES (2, 'the keg', 'restaurant');
-INSERT INTO to_dos (user_id, description, category) VALUES (2, 'macbook', 'product');
-INSERT INTO to_dos (user_id, description, category) VALUES (3, 'breaking bad', 'movies');
-INSERT INTO to_dos (user_id, description, category) VALUES (3, 'sapiens', 'books');
+INSERT INTO to_dos (user_id, description, category) VALUES (1, 'Harry Potter', 'movies');
+INSERT INTO to_dos (user_id, description, category) VALUES (1, 'To Kill a Mockingbird', 'books');
+INSERT INTO to_dos (user_id, description, category) VALUES (1, 'Outback Steakhouse', 'restaurant');
+INSERT INTO to_dos (user_id, description, category) VALUES (1, 'Soccer Ball', 'product');
+
+INSERT INTO to_dos (user_id, description, category) VALUES (2, 'The Keg', 'restaurant');
+INSERT INTO to_dos (user_id, description, category) VALUES (2, 'Macbook', 'product');
+
+INSERT INTO to_dos (user_id, description, category) VALUES (3, 'Breaking Bad', 'movies');
+INSERT INTO to_dos (user_id, description, category) VALUES (3, 'Sapiens', 'books');
